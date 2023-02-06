f229d80d94c2aaccd72430c1a37c21c2c1f18592
LiaPetrova <96750474+LiaPetrova@users.noreply.github.com>
2023-02-06 22:24:00 +0100

    React components

--- Intro/react-app/src/App.js
+++ Intro/react-app/src/App.js
@@ -1,28 +1,26 @@
-import { Hero } from './components/Hero';
-import { Welcome } from './components/Welcome';
-import { Service } from './components/Service';
-import { Contact } from './components/Contact';
-import { Footer } from './components/Footer';
-
+import { Hero } from "./components/Hero";
+import { Welcome } from "./components/Welcome";
+import { Service } from "./components/Service";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 function App() {
-  return (
-    <div>
+    return (
+        <div>
+            <Hero />
 
-        <Hero />
+            <Welcome />
 
-        <Welcome />
+            <Service />
 
-        <Service />
+            <Contact />
 
-        <Contact />
+            <Footer />
 
-        <Footer />
-
-
-  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
-  <script type="text/javascript" src="js/bootstrap.js"></script>
-</div>
+            <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+            <script type="text/javascript" src="js/bootstrap.js"></script>
+                
+    </div>
   );
 }
 
